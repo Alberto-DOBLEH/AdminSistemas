ad088bf57631e3fe2a4d8cd5a880f9955ee8db68
Alberto Hernandez <luis.alberto.hdez.hdez245@gmail.com>
2025-02-05 20:54:06 -0700

    Avances del dia de hoy
    hola

---
+++ Condicionales y Bucles.ps1
@@ -0,0 +1,20 @@
+﻿#Ilustracion14
+$condicion = $false
+if ( $condicion ){
+    Write-Output "La condicion es verdadera"
+}else{
+    Write-Output "La condicion es falsa"
+}
+
+#Ilustracion15
+$numero = 10
+
+if($numero -ge 3){
+    Write-Output "El numero [$numero] es mayor o igual que 3"
+}elseif($numero -lt 2){
+    Write-Output "El numero [$numero] es menor que 2"
+}else{
+    Write-Output "El numero [$numero] es igual a 2"
+}
+
+#Ilustracion17
---
+++ tipos_de_datos.ps1
@@ -0,0 +1,53 @@
+﻿#Ilustracion1
+$variable1 = "Hola"
+$variable2 = "Que Tal?"
+$variable3 = 100
+${var iable4} = 200
+
+#Ilustracion2
+new-variable -name var5 -value 400
+$var5
+
+#Ilustracion3
+$variable1
+$variable2
+$variable3
+${var iable4}
+
+#Ilustracion4
+$variable1+$variable2
+
+$variable3+${var iable4}
+$variable3-${var iable4}
+
+#Ilustracion5
+$variable1+$variable2
+$$
+
+#Ilustracion6
+$variable1+$variable2
+$^
+
+#Ilustracion7
+$variable1+$variable2
+$?
+
+#Ilustracion8
+$Error
+
+#Ilustracion9
+get-Help about_automatic_variables
+
+#Ilustracion10
+Get-Help about_preferences_variables
+
+#Ilustracion11
+[int]$var1 =100
+[string]$var2 = "hola"
+
+#Ilustracion12
+$var1.GetType()
+
+
+
+
