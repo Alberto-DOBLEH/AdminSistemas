7c5aaf0701f41d52e1289f08713bd6f660269cb4
Alberto Hernandez <luis.alberto.hdez.hdez245@gmail.com>
2025-02-22 08:41:33 -0700

    Ya se acabo esto
    Terminados lo Modulos de Linux
    Terminado el Script de SSH

--- P4.Servidor_SSH/scripssh.sh
+++ P4.Servidor_SSH/scripssh.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 source /home/albertodobleh/SysAdmin/AdminSistemas/LinModulos/modulos
 
+verde="\e[32m"
+rojo="\e[31m"
+reset="\e[0m"
+
 #Actualizar el sistema
-echo "Actualizando el sistema..."
+echo -e "${verde}Actualizando el sistema...${reset}"
 apt-get update -y
 apt-get upgrade -y
 
 #Instalar SSH
-echo "Instalando Servicio SSH..."
+echo .e "${verde}Instalando Servicio SSH...${reset}"
 apt-get install openssh-server -y
 
 #Solicitar la IP
 ipserver=""
 while :; do
     read -p "Ingrese la direccion del servidor: " ipserver
-    echo "$ipserver"
     if validar_ipv4 "$ipserver"; then
-        echo "La IP es válida."
-        break  # Sale del bucle si la IP es válida
+        echo "${verde}La IP es válida.${reset}"
+        break
     else
-        echo "IP inválida, inténtelo de nuevo."
+        echo "${rojo}IP inválida, inténtelo de nuevo.${reset}"
     fi
 done
 
 #Configuracion de IP Estatica
-echo -e "Empezando proceso de asignacion de ip estatica a la red local....."
-echo -e "Generando copia de seguridad del archivo de configuracion de NetPlan...."
+echo -e "${verde}Empezando proceso de asignacion de ip estatica a la red local.....${reset}"
+echo -e "${verde}Generando copia de seguridad del archivo de configuracion de NetPlan....${reset}"
 sudo cp /etc/netplan/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml.bak  # Copia de seguridad
 
-echo -e "Empezando la insercion de nueva informacion al archivo.."
+echo -e "${verde}Empezando la insercion de nueva informacion al archivo..${reset}"
 cat << EOF | sudo tee /etc/netplan/50-cloud-init.yaml
 network:
   version: 2
@@ -48,39 +51,53 @@ network:
 EOF
 
 # Aplicar cambios
-echo -e "Aplicando cambios en los adaptadores de red de la maquina...."
+echo -e "${verde}Aplicando cambios en los adaptadores de red de la maquina....${reset}"
 sudo netplan apply
 
 #Solicitud de datos para creacion de usuario
 usuario=""
-read -p "Ingrese el nombre del usuario: " usuario
+while true; do
+  read -p "Introduzca el nombre de usuario: " usuario
+  if [[ -n "$usuario" ]]; then
+    echo "${verde}Usuario Valido${reset}"
+    break
+  fi
+  echo "${rojo}El usuario no puede estar vacio${reset}"
+done
 contrasena=""
-read -p "Ingrese la contraseña: " contrasena
+while true; do
+  read -p "Introduzca la contrasena: " contrasena
+  if [[ -n "$contrasena" ]]; then
+    echo "${verde}Contrasena Valida${reset}"
+    break
+  fi
+  echo "${rojo}La contrasena no puede estar vacia${reset}"
+done
 
 #Crear usuario
-echo "Creando usuario..."
+echo "${verde}Creando usuario...${reset}"
 useradd -m -d /home/$usuario -s /bin/bash $usuario
 echo "$usuario:$contrasena" | chpasswd
 
 #Verificar que el usuario fue creado
-echo "Verificando que el usuario fue creado..."
+echo "${verde}Verificando que el usuario fue creado...${reset}"
 cat /etc/passwd | grep $usuario
 
 #Asignando permisos para el usuario en el SSH
-echo "Asignando permisos para el usuario en el SSH..."
+echo "${verde}Asignando permisos para el usuario en el SSH...${reset}"
 path="/home/$usuario/.ssh"
 mkdir -p $path
 chmod 700 $path
 chown $usuario:$usuario $path
 
 #Ajustando Firewall para permitir SSH
-echo "Ajustando Firewall para permitir SSH..."
+echo "${verde}Ajustando Firewall para permitir SSH...${reset}"
 ufw allow ssh
 
 #Iniciando el servicio SSH
-echo "Iniciando el servicio SSH..."
+echo "${verde}Iniciando el servicio SSH...${reset}"
 systemctl start ssh
 
 #Verificar que este corriendo el SSH
-echo "Verificando que el servicio SSH este corriendo..."
+echo "${verde}Verificando que el servicio SSH este corriendo...${reset}"
 systemctl status ssh
