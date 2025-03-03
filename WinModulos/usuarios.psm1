1760455156990e269372cd7eb3fd524510ca520d
Alberto Hernandez <luis.alberto.hdez.hdez245@gmail.com>
2025-03-02 19:26:48 -0700

    validador?

--- WinModulos/usuarios.psm1
+++ WinModulos/usuarios.psm1
@@ -141,11 +141,11 @@ function gestor_usuarios{
                         }
 
                         $vu3 = validar_usuario_existente -usuario $usuario
-                        if($vu3 -eq $true){
+                        if($vu3 -eq $false){
                             Write-Host "Error: El usuario no existe" -ForegroundColor Red
                             continue
                         }
-                    }While($vu1 -eq $false -or $vu2 -eq $false -or $vu3 -eq $true)
+                    }While($vu1 -eq $false -or $vu2 -eq $false -or $vu3 -eq $false)
 
                     try{
                         Write-Host "Eliminando usuario....." -ForegroundColor Green
