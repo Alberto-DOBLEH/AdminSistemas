0878e49756a76b2d7a7e169337598067fed90c9e
Alberto-DOBLEH <luis.alberto.hdez.hdez245@gmail.com>
2025-03-05 21:53:39 -0700

    ya que jale algo minimo

--- P5.Servidor_FTP/scriptftp.ps1
+++ P5.Servidor_FTP/scriptftp.ps1
@@ -16,7 +16,7 @@ Import-Module ../WinModulos/usuarios.psm1
     Install-WindowsFeature Web-FTP-Server -IncludeAllSubFeature -IncludeManagementTools
     Install-WindowsFeature Web-Server -IncludeAllSubFeature -IncludeManagementTools
     Install-WindowsFeature Web-Basic-Auth
-    #Install-WindowsFeature Web-Mngt-Server -IncludeManagementTools
+    Install-WindowsFeature Web-Mgmt-Server -IncludeManagementTools
     Import-Module WebAdministration -Force
 
     # Firewall rule
