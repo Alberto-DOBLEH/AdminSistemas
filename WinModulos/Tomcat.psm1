1665da308e14c4a1f29a118ec6484cb58b68fcbd
Alberto-DOBLEH <luis.alberto.hdez.hdez245@gmail.com>
2025-03-18 21:27:24 -0700

    jdk

--- WinModulos/Tomcat.psm1
+++ WinModulos/Tomcat.psm1
@@ -57,7 +57,7 @@ function tomcat(){
     # 7. Iniciar Apache Tomcat
     #Start-Process "$extractPath\apache-tomcat-$version\bin\startup.bat"
 
-    Set-Location "$extractPath\apache-tomcat-$version\bin"
+    cd "$extractPath\apache-tomcat-$version\bin"
 
     .\service.bat install tomcat$v
     Start-Service -Name tomcat$v
