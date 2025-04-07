2fe0bb41fa1d63196fce3c28cbd9114d193209e0
Alberto-DOBLEH <luis.alberto.hdez.hdez245@gmail.com>
2025-04-06 18:29:48 -0700

    me pase de tonto

--- P8.Servidor_SMTP/scriptsmtp.ps1
+++ P8.Servidor_SMTP/scriptsmtp.ps1
@@ -2,7 +2,7 @@
 $downloadPath = "https://download-us.pmail.com/m32-480.exe"
 $downloadedPath = "$env:HOMEPATH\Downloads\mercury.exe"
 
-Invoke_WebRequest -Uri $downloadPath -Outfile $downloadedPath -UseBasicParsing -ErrorAction Stop
+Invoke-WebRequest -Uri $downloadPath -Outfile $downloadedPath -UseBasicParsing -ErrorAction Stop
 cd $env:HOMEPATH\Downloads
 Start-Process .\mercury.exe
 
