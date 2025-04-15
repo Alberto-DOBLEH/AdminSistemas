b2845a1cd34df3fe38b6b018fd7dfb1464c3012d
Alberto-DOBLEH <luis.alberto.hdez.hdez245@gmail.com>
2025-04-15 16:09:40 -0700

    vamos a probar los usuarios del AD

--- WinModulos/usuarios_ad.psm1
+++ WinModulos/usuarios_ad.psm1
@@ -1,5 +1,40 @@
 function crear_user_ad{
+    Write-Host "---Seccion de creacion de usuarios---"
 
+    $user = Read-Host "Ingresa el nombre del usuario"
+
+    $password = Read-Host "Ingresa la contraseña del usuario" -AsSecureString
+
+
+    #Menu de seleccion de OU
+    do{
+        Write-Host "Selecciona la OU donde se creara el usuario"
+        Write-Host "1. OU cuates"
+        Write-Host "2. OU no cuates"
+        $opcou = Read-Host "Ingresa la OU <1/2>"
+        switch($ocpou){
+            1{
+                $ou = "cuates"
+                Write-Host "Seleccionaste la OU cuates"
+            }
+            2{
+                $ou = "no cuates"
+                Write-Host "Seleccionaste la OU no cuates"
+            }
+            default{
+                Write-Host "Opcion no valida, selecciona una opcion valida" -ForegroundColor Red
+            }
+        }
+
+    $ouPath = "OU=$ou,DC=cuates,DC=local"
+    }while($opcou -ne 1 -and $opcou -ne 2)
+    
+    try{
+        New-ADUser -Name $user -SamAccountName $user -AccountPassword $password -Enabled $true -Path $ouPath
+        Write-Host "Usuario $user creado exitosamente en la OU $ou" -ForegroundColor Green
+    }catch{
+        Write-Host "Error al crear el usuario: $_" -ForegroundColor Red
+    }
 }
 function Modificar_user_ad{
 
