5cd28f8d9a21818cad2e9ab5f1c040b95fa0ea60
Alberto-DOBLEH <luis.alberto.hdez.hdez245@gmail.com>
2025-05-18 20:47:27 -0700

    V6

--- P9.P10.Servidor_AD/scriptad.ps1
+++ P9.P10.Servidor_AD/scriptad.ps1
@@ -98,7 +98,7 @@ if ($fsrmFeature.Installed) {
 $plantillaCuates = Get-FsrmQuotaTemplate -Name "Cuota_Cuates" -ErrorAction SilentlyContinue
 if (-not $plantillaCuates) {
     Write-Host "Creando plantilla de cuota para Cuates (5MB)..." -ForegroundColor Yellow
-    New-FsrmQuotaTemplate -Name "Cuota_Cuates" -Description "Límite de 5MB para usuarios de la OU Cuates" -Size 5MB
+    New-FsrmQuotaTemplate -Name "Cuota_Cuates" -Description "Límite de 5MB para usuarios de la OU Cuates" -Size 5MB -SoftLimit
 } else {
     Write-Host "La plantilla 'Cuota_Cuates' ya existe." -ForegroundColor Green
 }
@@ -107,7 +107,7 @@ if (-not $plantillaCuates) {
 $plantillaNoCuates = Get-FsrmQuotaTemplate -Name "Cuota_NoCuates" -ErrorAction SilentlyContinue
 if (-not $plantillaNoCuates) {
     Write-Host "Creando plantilla de cuota para NoCuates (10MB)..." -ForegroundColor Yellow
-    New-FsrmQuotaTemplate -Name "Cuota_NoCuates" -Description "Límite de 10MB para usuarios de la OU NoCuates" -Size 10MB
+    New-FsrmQuotaTemplate -Name "Cuota_NoCuates" -Description "Límite de 10MB para usuarios de la OU NoCuates" -Size 10MB -SoftLimit
 } else {
     Write-Host "La plantilla 'Cuota_NoCuates' ya existe." -ForegroundColor Green
 }
