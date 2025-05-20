ba9b56d37eb9bbba4fb4008049f23a06361e0dd3
Alberto-DOBLEH <luis.alberto.hdez.hdez245@gmail.com>
2025-05-20 14:49:54 -0700

    Docker?

--- P11.Docker/scriptdock.sh
+++ P11.Docker/scriptdock.sh
@@ -15,8 +15,8 @@ echo "Creando imagen personalizada de Apache con index.html modificado..."
 mkdir -p apache_custom
 cat <<EOF > apache_custom/index.html
 <html>
-  <head><title>Servidor Apache Personalizado</title></head>
-  <body><h1>¡Hola desde la imagen personalizada!</h1></body>
+  <head><title>Servidor Apache de Docker</title></head>
+  <body><h1>Apache desde Docker</h1></body>
 </html>
 EOF
 
@@ -35,14 +35,20 @@ docker network create app_network
 echo "Levantando contenedor Apache personalizado..."
 docker run -dit --name apache_server --network app_network -p 8080:80 apache_custom:1.0
 
+read -p "Usuario para el contenedor PostgreSQL (usuario1): " USUARIO1
+read -p "Contraseña para el contenedor PostgreSQL (Hola9080): " CONTRASENA1
+
+read -p "Usuario para el contenedor PostgreSQL (usuario2): " USUARIO2
+read -p "Contraseña para el contenedor PostgreSQL (Hola9080): " CONTRASENA1
+
 # Crear dos contenedores con PostgreSQL
 echo "Levantando contenedores PostgreSQL..."
 docker run -dit --name pg1 --network app_network \
-  -e POSTGRES_PASSWORD=clave123 -e POSTGRES_USER=usuario1 -e POSTGRES_DB=bd1 \
+  -e POSTGRES_PASSWORD=$CONTRASENA1 -e POSTGRES_USER=$USUARIO1 -e POSTGRES_DB=bd1 \
   postgres:latest
 
 docker run -dit --name pg2 --network app_network \
-  -e POSTGRES_PASSWORD=clave123 -e POSTGRES_USER=usuario2 -e POSTGRES_DB=bd2 \
+  -e POSTGRES_PASSWORD=$CONTRASENA2 -e POSTGRES_USER=$USUARIO2 -e POSTGRES_DB=bd2 \
   postgres:latest
 
 # Esperar a que PostgreSQL se inicie
@@ -54,6 +60,4 @@ echo "Instalando cliente psql en pg1 para pruebas de conexión..."
 docker exec pg1 apt update && docker exec pg1 apt install -y postgresql-client
 
 echo "Probando conexión desde pg1 a pg2..."
-docker exec pg1 bash -c "PGPASSWORD=clave123 psql -h pg2 -U usuario2 -d bd2 -c '\l'"
-
-echo "Script completado."
+docker exec pg1 psql -h pg2 -U $USUARIO2 -d bd2 -c '\l'
