876e5fb60cc15b56659ba2e34eb6b8d2322a2762
Alberto Hernandez <luis.alberto.hdez.hdez245@gmail.com>
2025-06-24 13:59:25 -0700

    y si cambio las credenciales?

--- P7.Servidor_SSL/http.ps1
+++ P7.Servidor_SSL/http.ps1
@@ -92,7 +92,7 @@ function listarDirectoriosFtp {
 
         $request = [System.Net.FtpWebRequest]::Create($servidorFtp)
         $request.Method = [System.Net.WebRequestMethods+FTP]::ListDirectory
-        $request.Credentials = New-Object System.Net.NetworkCredential("alberto", "Hola9080@")
+        $request.Credentials = New-Object System.Net.NetworkCredential("anonymous", "anonymous@example.com")
         $request.EnableSsl = $true
         $request.UsePassive = $true
         $request.UseBinary = $true
