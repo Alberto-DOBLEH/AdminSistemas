c73cc20ae2b2f35ca3a3b2b24b7a94dae094b0f3
Alberto Hernandez <luis.alberto.hdez.hdez245@gmail.com>
2025-06-23 21:37:02 -0700

    y ahora si jala?

--- P7.Servidor_SSL/http.ps1
+++ P7.Servidor_SSL/http.ps1
@@ -6,7 +6,7 @@ $ProgressPreference = 'SilentlyContinue'
 new-item -Path "C:\descargas" -ItemType Directory -Force
 $opcDescarga = Read-Host "Desde donde quieres realizar la instalacion de los servicios? (web/ftp)"
 
-$servidorFtp = "ftps://127.0.0.1:990/"
+$servidorFtp = "ftp://127.0.0.1/"
 
 function Es-PuertoValido([int]$puerto) {
     $puertosReservados = @{
