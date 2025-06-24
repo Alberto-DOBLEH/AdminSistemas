6d081d7c791891c7e2acff00a811c707951447b7
Alberto Hernandez <luis.alberto.hdez.hdez245@gmail.com>
2025-06-23 21:57:36 -0700

    y ahora si jala?

--- P7.Servidor_SSL/ftp.ps1
+++ P7.Servidor_SSL/ftp.ps1
@@ -181,9 +181,9 @@ $param3 =@{
 
 Add-WebConfiguration @param3
 
-icacls "C:\FTP\" /grant "IUSR:(OI)(CI)F"
+icacls "C:\FTP\" /grant "IIS_IUSR:(OI)(CI)F"
 icacls "C:\FTP\" /grant "Todos:(OI)(CI)F"
-icacls "C:\FTP\LocalUser\Public\General" /grant "IUSR:(OI)(CI)F"
+icacls "C:\FTP\LocalUser\Public\General" /grant "IIS_IUSR:(OI)(CI)F"
 icacls "C:\FTP\LocalUser\Public\General" /grant "Todos:(OI)(CI)F"
 
 $opcSsl = Read-Host "Desea activar SSL?"
